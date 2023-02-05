68f4dc62a36314e4f9927605bb43d3ae98481bfa
almas <ntldr@mail.ru>
2023-02-05 19:43:09 +0600

    axios added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product';
-import { products } from './data/products';
+//import { products } from './data/products';
+import axios from 'axios'
+import { IProduct } from './model';
 //import logo from './logo.svg';
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  async function fetchProducts() {
+    const res = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=7');
+    console.log(res);
+  }
+  
+  
+  //const [count, setCount] = useState(0);
+  useEffect(()=>{
+    console.log('effect');
+    fetchProducts();
+  }, [])
+
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-    <h1 className='font-bold'>Hello react {count}</h1>
-    <button className='py-2 px-4 border' onClick={()=> setCount(count + 1)}> Test TSX </button>
-    <Product product={products[0]} title={'test'} />
+    {/* <h1 className='font-bold'>Hello react {count}</h1>
+    <button className='py-2 px-4 border' onClick={()=> setCount(count + 1)}> Test TSX </button> */}
+      {/* {products.forEach(element => {
+        <Product product={element} key={element.id} />
+      })} */}
+      {products.map(element => <Product product={element} key={element.id} />)}
     </div>
   );
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { IProduct } from "../model";
 
+interface ProductProps {
+    product: IProduct
+}
 
-export function Product(props: any ) {
-    console.log(props);
+export function Product(props: ProductProps ) {
+
+    const [details, setDetails] = useState(false);
+    const btnClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
+    const btnAfterClassName = ['py-2 px-4 border', btnClassName];
+
+    //console.log(props);
     return (
 
+        
         <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-            Product!
+            <img src={props.product.image} alt={props.product.title} className="w-1/6"></img>
+            <p>Product: {props.product.title}</p>
+            <p className="font-bold">{props.product.price}</p>
+            <button 
+                className ={btnAfterClassName.join(' ')}
+                onClick={()=> {setDetails(prev => !prev)}}
+                >
+                {details ? 'hide details' : 'show details'}  
+            </button>
+            {details && <div>
+                <p>{ props.product.description }</p>
+                <p>Rate: <span style={{fontWeight:'bold'}}>{ props.product.rating.rate}</span></p>
+            </div>} 
         </div>
 
 
